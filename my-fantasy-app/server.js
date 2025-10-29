0551f24be73945e85dacd399099321aeb25e8c70
sobasobasobasoba <jonathan.schuller3@gmail.com>
2025-10-29 14:59:30 -0400

    parallel queries

--- my-fantasy-app/server.js
+++ my-fantasy-app/server.js
@@ -35,6 +35,7 @@
 import express from "express";
 //import cors from "cors";
 import mysql from "mysql2";
+import async from "async";
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -42,24 +43,35 @@ const app = express();
 app.use(express.json());
 
 // create db connection
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Teddy1065Dredge!",   // ← replace with your MySQL password
-  database: "monitoring_redefined" // ← replace with your DB name
-});
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "Teddy1065Dredge!",   // ← replace with your MySQL password
+//   database: "monitoring_redefined" // ← replace with your DB name
+// });
+
+var db = mysql.createPool({
+    connectionLimit: 10,
+    host: "localhost",
+    user: "root",
+    password: "Teddy1065Dredge!",   // ← replace with your MySQL password
+    database: "monitoring_redefined" // ← replace with your DB name
+})
 
 // connect to db
-db.connect((err) => {
-  if (err) {
-    console.error("Error connecting to MySQL:", err);
-  } else {
-    console.log("Connected to MySQL database");
-  }
-});
+
 
 // simple GET endpoint
 app.get("/api/matchups", (req, res) => {
+    db.connect((err) => {
+        if (err) {
+            console.error("Error connecting to MySQL:", err);
+            res.set({"Access-Control-Allow-Origin": "*"});
+            res.status(500).json({error: "Can't connect to database"});
+        } else {
+            console.log("Connected to MySQL database");
+        }
+    });
     var query = "SELECT * FROM monitoring_redefined.matchups;";
     db.query(query, (err, results) => {
         if (err) {
@@ -75,6 +87,15 @@ app.get("/api/matchups", (req, res) => {
 
 // return all matchups from this team
 app.get("/api/team", (req, res) => {
+    db.connect((err) => {
+        if (err) {
+            console.error("Error connecting to MySQL:", err);
+            res.set({"Access-Control-Allow-Origin": "*"});
+            res.status(500).json({error: "Can't connect to database"});
+        } else {
+            console.log("Connected to MySQL database");
+        }
+    });
     var team = req.query.team;
     console.log("TEAM");
     console.log(team);
@@ -99,36 +120,71 @@ app.get("/api/team", (req, res) => {
 });
 
 app.get("/api/records/matchup", (req, res) => {
+    db.connect((err) => {
+        if (err) {
+            console.error("Error connecting to MySQL:", err);
+            res.set({"Access-Control-Allow-Origin": "*"});
+            res.status(500).json({error: "Can't connect to database"});
+        } else {
+            console.log("Connected to MySQL database");
+        }
+    });
     var blowoutQuery = "SELECT *, winningTeamPoints - losingTeamPoints AS point_diff FROM monitoring_redefined.matchups ORDER BY point_diff DESC LIMIT 10;";
     var closestQuery = "SELECT *, winningTeamPoints - losingTeamPoints AS point_diff FROM monitoring_redefined.matchups ORDER BY point_diff ASC LIMIT 10;";
     let queryOutput = {};
-    db.query(blowoutQuery, (err, results) => {
-        if (err) {
-            console.error("Database query error:", err);
-            res.set({"Access-Control-Allow-Origin": "*"})
-            res.status(500).json({ error: "Database error" });
-        }   else {
-            console.log(results);
-            queryOutput["blowout"] = results;
-            //res.set({"Access-Control-Allow-Origin": "*"})
-            //res.json(results);
-        }
+
+    async.parallel([
+       function(parallel_done) {
+           pool.query(blowoutQuery, {}, function(err, results) {
+               if (err) return parallel_done(err);
+               queryOutput.blowout = results;
+               console.log(results);
+               parallel_done();
+           });
+       },
+       function(parallel_done) {
+           pool.query(closestQuery, {}, function(err, results) {
+               if (err) return parallel_done(err);
+               queryOutput.closest = results;
+               console.log(results);
+               parallel_done();
+           });
+       }
+    ], function(err) {
+         if (err) console.log(err);
+         pool.end();
+         console.log("parallel_done");
+         console.log(queryOutput);
+         res.set({"Access-Control-Allow-Origin": "*"});
+         res.send(queryOutput);
     });
-    db.query(closestQuery, (err, results) => {
-        if (err) {
-            console.error("Database query error:", err);
-            res.set({"Access-Control-Allow-Origin": "*"})
-            res.status(500).json({ error: "Database error" });
-        }   else {
-            console.log(results);
-            queryOutput["closest"] = results;
-            //res.set({"Access-Control-Allow-Origin": "*"})
-            //res.json(results);
-        }
-    });
-    console.log(queryOutput);
-    res.set({"Access-Control-Allow-Origin": "*"});
-    res.json(queryOutput);
+    // db.query(blowoutQuery, (err, results) => {
+    //     if (err) {
+    //         console.error("Database query error:", err);
+    //         res.set({"Access-Control-Allow-Origin": "*"})
+    //         res.status(500).json({ error: "Database error" });
+    //     }   else {
+    //         console.log(results);
+    //         queryOutput["blowout"] = results;
+    //         //res.set({"Access-Control-Allow-Origin": "*"})
+    //         //res.json(results);
+    //     }
+    // });
+    // db.query(closestQuery, (err, results) => {
+    //     if (err) {
+    //         console.error("Database query error:", err);
+    //         res.set({"Access-Control-Allow-Origin": "*"})
+    //         res.status(500).json({ error: "Database error" });
+    //     }   else {
+    //         console.log(results);
+    //         queryOutput["closest"] = results;
+    //         //res.set({"Access-Control-Allow-Origin": "*"})
+    //         //res.json(results);
+    //     }
+    // });
+    // console.log(queryOutput);
+    // res.set({"Access-Control-Allow-Origin": "*"});
+    // res.json(queryOutput);
 
 });
 
