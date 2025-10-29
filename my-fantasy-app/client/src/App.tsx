c3ea5e8b92c6d41c006506a521128664dcc4e59f
sobasobasobasoba <jonathan.schuller3@gmail.com>
2025-10-29 18:43:08 -0400

    adding champ banners

--- my-fantasy-app/client/src/App.tsx
+++ my-fantasy-app/client/src/App.tsx
@@ -408,7 +408,7 @@ function TeamPage() {
           {team.championships.map((c, i) => (
             <img 
               key={i}
-              className="w-20 h-36 rounded-lg object-cover"
+              className="w-26 h-46 rounded-lg object-cover"
               src={c.banner}
               alt={`Championship ${i+1}`}
             />
