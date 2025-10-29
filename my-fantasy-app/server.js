0b9728f70454fadd16488219ecf39210f0f0baa9
sobasobasobasoba <jonathan.schuller3@gmail.com>
2025-10-29 15:07:13 -0400

    cleanup

--- my-fantasy-app/server.js
+++ my-fantasy-app/server.js
@@ -1,37 +1,3 @@
-//const express = import('express');
-//const bodyParser = import('body-parser');
-//const mysql = import('mysql2');
-// import express from 'express';
-// import bodyParser from 'body-parser';
-// import mysql from 'mysql2';
-
-
-// const app = express();
-// const port = process.env.PORT || 5000;
-
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended: true}));
-
-// const con = await mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     password: "Teddy1065Dredge!"
-// });
-
-// app.get('/api/hello', async (req, res) => {
-//     var query = "SELECT * FROM monitoring_redefined.matchups;";
-//     try {
-//         const [rows] = await con.query(query);
-//         res.set({"Access-Control-Allow-Origin": "*"})
-//         res.json(rows);
-//     } catch (err) {
-//         console.error(err);
-//         res.status(500).json({error: 'Database error'});
-//     }
-// });
-
-// app.listen(port, () => console.log(`Listening on port ${port}`));
-
 import express from "express";
 //import cors from "cors";
 import mysql from "mysql2";
@@ -42,13 +8,6 @@ const app = express();
 //app.use(cors());
 app.use(express.json());
 
-// create db connection
-// const db = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "Teddy1065Dredge!",   // ← replace with your MySQL password
-//   database: "monitoring_redefined" // ← replace with your DB name
-// });
 
 var db = mysql.createPool({
     connectionLimit: 10,
@@ -63,16 +22,10 @@ var db = mysql.createPool({
 
 // simple GET endpoint
 app.get("/api/matchups", (req, res) => {
-    // db.connect((err) => {
-    //     if (err) {
-    //         console.error("Error connecting to MySQL:", err);
-    //         res.set({"Access-Control-Allow-Origin": "*"});
-    //         res.status(500).json({error: "Can't connect to database"});
-    //     } else {
-    //         console.log("Connected to MySQL database");
-    //     }
-    // });
+
     var query = "SELECT * FROM monitoring_redefined.matchups;";
+    res.set({"Access-Control-Allow-Origin": "*"});
+    
     db.query(query, (err, results) => {
         if (err) {
             console.error("Database query error:", err);
@@ -87,18 +40,13 @@ app.get("/api/matchups", (req, res) => {
 
 // return all matchups from this team
 app.get("/api/team", (req, res) => {
-    // db.connect((err) => {
-    //     if (err) {
-    //         console.error("Error connecting to MySQL:", err);
-    //         res.set({"Access-Control-Allow-Origin": "*"});
-    //         res.status(500).json({error: "Can't connect to database"});
-    //     } else {
-    //         console.log("Connected to MySQL database");
-    //     }
-    // });
+
     var team = req.query.team;
     console.log("TEAM");
     console.log(team);
+    res.set({"Access-Control-Allow-Origin": "*"});
+
+
     if (!(/^[a-zA-Z]+$/.test(team))){
         console.error("Not a real team");
         res.set({"Access-Control-Allow-Origin": "*"})
@@ -120,18 +68,11 @@ app.get("/api/team", (req, res) => {
 });
 
 app.get("/api/records/matchup", (req, res) => {
-    // db.connect((err) => {
-    //     if (err) {
-    //         console.error("Error connecting to MySQL:", err);
-    //         res.set({"Access-Control-Allow-Origin": "*"});
-    //         res.status(500).json({error: "Can't connect to database"});
-    //     } else {
-    //         console.log("Connected to MySQL database");
-    //     }
-    // });
+
     var blowoutQuery = "SELECT *, winningTeamPoints - losingTeamPoints AS point_diff FROM monitoring_redefined.matchups ORDER BY point_diff DESC LIMIT 10;";
     var closestQuery = "SELECT *, winningTeamPoints - losingTeamPoints AS point_diff FROM monitoring_redefined.matchups ORDER BY point_diff ASC LIMIT 10;";
     let queryOutput = {};
+    res.set({"Access-Control-Allow-Origin": "*"});
 
     async.parallel([
        function(parallel_done) {
@@ -157,33 +98,6 @@ app.get("/api/records/matchup", (req, res) => {
          res.set({"Access-Control-Allow-Origin": "*"});
          res.send(queryOutput);
     });
-    // db.query(blowoutQuery, (err, results) => {
-    //     if (err) {
-    //         console.error("Database query error:", err);
-    //         res.set({"Access-Control-Allow-Origin": "*"})
-    //         res.status(500).json({ error: "Database error" });
-    //     }   else {
-    //         console.log(results);
-    //         queryOutput["blowout"] = results;
-    //         //res.set({"Access-Control-Allow-Origin": "*"})
-    //         //res.json(results);
-    //     }
-    // });
-    // db.query(closestQuery, (err, results) => {
-    //     if (err) {
-    //         console.error("Database query error:", err);
-    //         res.set({"Access-Control-Allow-Origin": "*"})
-    //         res.status(500).json({ error: "Database error" });
-    //     }   else {
-    //         console.log(results);
-    //         queryOutput["closest"] = results;
-    //         //res.set({"Access-Control-Allow-Origin": "*"})
-    //         //res.json(results);
-    //     }
-    // });
-    // console.log(queryOutput);
-    // res.set({"Access-Control-Allow-Origin": "*"});
-    // res.json(queryOutput);
 
 });
 
